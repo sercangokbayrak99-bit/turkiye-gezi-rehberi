0392c791d1d0f59f8986fa6228f8d8907e344b4c
Hidro Seal <hidroseal@Hidro-MacBook-Air.local>
2026-08-27 14:08:27 +0300

    Deploy Catalca and Silivri quality update

--- service-worker.js
+++ service-worker.js
@@ -1,6 +1,6 @@
-const CACHE_NAME = 'turkiye-rehberi-1787828550970';
+const CACHE_NAME = 'turkiye-rehberi-1787828883531';
 const APP_ASSETS = [
-  "./_expo/static/js/web/AppEntry-d713577e48408fb119114e78d071e129.js",
+  "./_expo/static/js/web/AppEntry-648a4efe6549f447759f14df0684f755.js",
   "./assets/assets/baths/emir-sultan-hamam.13bd41db784754b82f232221cc4a963e.jpg",
   "./assets/assets/baths/eski-kaplica.1ea83cf46b353d2d85249db3024e257b.jpg",
   "./assets/assets/baths/muradiye-hamam.1d48358fb4ec87be3cda0ab4a1d4f2fc.jpg",
@@ -18,11 +18,14 @@ const APP_ASSETS = [
   "./assets/assets/istanbul/balat.27dab1647f8c0fd15d7d91131b02863b.jpg",
   "./assets/assets/istanbul/beaches/alacali.7a680bb19756dd83293354617bdeb3cb.jpg",
   "./assets/assets/istanbul/beaches/buyukcekmece.c769c416553c71a6bb01e63de0551f74.jpg",
+  "./assets/assets/istanbul/beaches/cilingoz.d860c8c844785f7731fcad3f88acf8aa.jpg",
+  "./assets/assets/istanbul/beaches/gumusyaka.15d23a4e8501af2d9fb84ca0080c7c9d.jpg",
   "./assets/assets/istanbul/beaches/gurpinar.9a66743c3b521c75a33dc236b10eed9b.jpg",
   "./assets/assets/istanbul/beaches/imrenli.841f57eed11d598484724faecda6a770.jpg",
   "./assets/assets/istanbul/beaches/kadirga-koyu.c6a03491a28c12f6305b7916cf207233.jpg",
   "./assets/assets/istanbul/beaches/kilimli-koyu.9eb671284d36d790396aa3133325e06a.jpg",
   "./assets/assets/istanbul/beaches/kinaliada-coast.32c40f43af53be9dee979c5f0736685e.jpg",
+  "./assets/assets/istanbul/beaches/kumburgaz.2475d87b027782cb100fc9a3e9b35c4f.jpg",
   "./assets/assets/istanbul/beaches/sile-beach.e7635aab8a69562bf0c79c8aa53cc2da.jpg",
   "./assets/assets/istanbul/beaches/yalikoy.21c5884150aed4c96b929b8ce7c8dc82.jpg",
   "./assets/assets/istanbul/belgrad-forest-new.ad9b23b5de4791b443ebf24431834000.jpg",
