3fa2bf608fa27e603749671c664d67a30198514b
Hidro Seal <hidroseal@Hidro-MacBook-Air.local>
2026-08-28 23:25:58 +0300

    Deploy Izmir hero sizing fix

--- service-worker.js
+++ service-worker.js
@@ -1,6 +1,6 @@
-const CACHE_NAME = 'turkiye-rehberi-1787948518780';
+const CACHE_NAME = 'turkiye-rehberi-1787948748751';
 const APP_ASSETS = [
-  "./_expo/static/js/web/AppEntry-500b5c5af87127c183a408c3ef48e106.js",
+  "./_expo/static/js/web/AppEntry-17fd6980d4f6bd7d295c70b83e980663.js",
   "./assets/assets/ankara/altinpark.3c175c24854b56b0b1c1461158203ff1.jpg",
   "./assets/assets/ankara/anadolu-medeniyetleri.4cffabc67dbb9d0d5a7f56e962907567.jpg",
   "./assets/assets/ankara/anitkabir.d9a2810f2ea2d241decd9532b5e76f22.jpg",
