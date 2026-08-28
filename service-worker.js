0ca9d1740bd20898a892fe87f2bf3dd0a23b809b
Hidro Seal <hidroseal@Hidro-MacBook-Air.local>
2026-08-28 23:28:12 +0300

    Deploy final Izmir hero framing

--- service-worker.js
+++ service-worker.js
@@ -1,6 +1,6 @@
-const CACHE_NAME = 'turkiye-rehberi-1787948748751';
+const CACHE_NAME = 'turkiye-rehberi-1787948881825';
 const APP_ASSETS = [
-  "./_expo/static/js/web/AppEntry-17fd6980d4f6bd7d295c70b83e980663.js",
+  "./_expo/static/js/web/AppEntry-c174d5e090b5d923f5fb9212e2fa319f.js",
   "./assets/assets/ankara/altinpark.3c175c24854b56b0b1c1461158203ff1.jpg",
   "./assets/assets/ankara/anadolu-medeniyetleri.4cffabc67dbb9d0d5a7f56e962907567.jpg",
   "./assets/assets/ankara/anitkabir.d9a2810f2ea2d241decd9532b5e76f22.jpg",
