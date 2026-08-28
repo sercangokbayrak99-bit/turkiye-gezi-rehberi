fa94178303fcb75062fb6d0aa6d10c881d6b3ad8
Hidro Seal <hidroseal@Hidro-MacBook-Air.local>
2026-08-29 00:55:40 +0300

    Deploy 281c58e

--- service-worker.js
+++ service-worker.js
@@ -1,6 +1,6 @@
-const CACHE_NAME = 'turkiye-rehberi-1787953509059';
+const CACHE_NAME = 'turkiye-rehberi-1787954116418';
 const APP_ASSETS = [
-  "./_expo/static/js/web/AppEntry-2c923c706c10dca77c6fb95783cb6180.js",
+  "./_expo/static/js/web/AppEntry-fbff8c24de48ca995e22af19f8642211.js",
   "./assets/assets/ankara/altinpark.3c175c24854b56b0b1c1461158203ff1.jpg",
   "./assets/assets/ankara/anadolu-medeniyetleri.4cffabc67dbb9d0d5a7f56e962907567.jpg",
   "./assets/assets/ankara/anitkabir.d9a2810f2ea2d241decd9532b5e76f22.jpg",
