354d3e2dee286ca1995c3141f437951c5a23f203
Hidro Seal <hidroseal@Hidro-MacBook-Air.local>
2026-08-28 11:29:31 +0300

    Deploy f721325

--- service-worker.js
+++ service-worker.js
@@ -1,6 +1,6 @@
-const CACHE_NAME = 'turkiye-rehberi-1787865402908';
+const CACHE_NAME = 'turkiye-rehberi-1787905762132';
 const APP_ASSETS = [
-  "./_expo/static/js/web/AppEntry-2492e561d81e6ea9bdc5e3779a3dfed4.js",
+  "./_expo/static/js/web/AppEntry-c743f4c22517789169e1016267780212.js",
   "./assets/assets/baths/emir-sultan-hamam.13bd41db784754b82f232221cc4a963e.jpg",
   "./assets/assets/baths/eski-kaplica.1ea83cf46b353d2d85249db3024e257b.jpg",
   "./assets/assets/baths/muradiye-hamam.1d48358fb4ec87be3cda0ab4a1d4f2fc.jpg",
