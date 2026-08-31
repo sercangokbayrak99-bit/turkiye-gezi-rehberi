b8c8d59780004e1e0bca3ed78793919f9b2344dc
Hidro Seal <hidroseal@Hidro-MacBook-Air.local>
2026-08-31 20:48:38 +0300

    Deploy Izmir district guides

--- service-worker.js
+++ service-worker.js
@@ -1,6 +1,6 @@
-const CACHE_NAME = 'turkiye-rehberi-1788195007165';
+const CACHE_NAME = 'turkiye-rehberi-1788198440313';
 const APP_ASSETS = [
-  "./_expo/static/js/web/AppEntry-1d4d7abc146c56c629ccbe7e9e0eb862.js",
+  "./_expo/static/js/web/AppEntry-0d55d54aee41436e7016b143cdbde78c.js",
   "./assets/assets/ankara/altinpark.3c175c24854b56b0b1c1461158203ff1.jpg",
   "./assets/assets/ankara/anadolu-medeniyetleri.4cffabc67dbb9d0d5a7f56e962907567.jpg",
   "./assets/assets/ankara/anitkabir.d9a2810f2ea2d241decd9532b5e76f22.jpg",
